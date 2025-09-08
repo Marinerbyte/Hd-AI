9d7b630e5e1925b3ac7795df809d652c46e36425
Marinerbyte <yasinkarani14@gmail.com>
2025-09-08 12:48:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ class Config:
     FLASK_SECRET_KEY = os.getenv("FLASK_SECRET_KEY", "a-very-secret-flask-key")
     HARDCODED_MASTER = "yasin"
     LOGIN_URL = "https://api.howdies.app/login"
-    WS_URL = "wss://app.howdies.app/"
+    WS_URL = "wss://howdies.app:3000/"
     ROOM_JOIN_DELAY_SECONDS = 2
     REJOIN_ON_KICK_DELAY_SECONDS = 3
     INITIAL_RECONNECT_DELAY = 10
